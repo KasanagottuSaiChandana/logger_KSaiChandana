a88a40954360b6a56723acb97b81c027c1cd7ffd
KasanagottuSaiChandana <saichandhana04@gmail.com>
2020-02-26 23:07:27 +0530

    first commit

---
+++ main/java/com/task/task5/Main.java
@@ -0,0 +1,13 @@
+package com.task.task5;
+import org.apache.log4j.Logger;
+
+
+public class Main {
+	static Logger log = Logger.getLogger(Main.class.getName());
+    public static void main( String[] args )
+    {
+    	Interest.calculateInterest();
+        Construction.costEvaluator();
+    }
+
+}
